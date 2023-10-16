22b1a0a89788ab29dbd33dd6af8607ace1199b92
Shashank Naik <shashanknaik808@gmail.com>
2023-10-16 13:54:36 +0530

    Getting the random word

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 const initGame = () => {
-    //let randomObj = words
+    let randomObj = words[Math.floor(Math.random() * words.length)];
+    let wordArray = randomObj.word.split("");
+    console.log(wordArray);
 }
+initGame();
