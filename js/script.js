7edb02c1ab16b3cc918ca1bb8288d3c43cbd2a0f
Shashank Naik <shashanknaik808@gmail.com>
2023-10-16 13:59:02 +0530

    Shuffling/arranging word randomly

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
+const wordText = document.querySelector(".word");
+
 const initGame = () => {
+
     let randomObj = words[Math.floor(Math.random() * words.length)];
     let wordArray = randomObj.word.split("");
-    console.log(wordArray);
+
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+    }
+
+    wordText.innerText = wordArray.join("");
+    console.log(randomObj);
 }
 initGame();
